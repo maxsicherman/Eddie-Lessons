4c6a0aee08ddead4105cb765c8faf15a56c4a444
maxsicherman <97261414+maxsicherman@users.noreply.github.com>
2022-06-18 10:47:35 -0700

    Add files via upload

---
+++ edlesson.py
@@ -0,0 +1,27 @@
+kanye = "jean-yuhs"
+integer = 1
+double = 1.2
+char = 'a'
+boolean = False
+deeznuts = [1,2,3,4,5]
+goteem = deeznuts[0] + deeznuts[4]
+players = ['Max', 'Eddie', 'Lila']
+current_player = 'Max'
+
+print(15%2)
+
+# if (not boolean) == True:
+#     print('False')
+# else:
+#     print('True')
+
+
+# def plus(x, y):
+#     return(x+y)
+
+# sum = plus(6, 12)
+
+# while integer < 10:
+#     integer = integer + 1
+#     print(integer)
+
